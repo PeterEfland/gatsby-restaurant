91755d9c050c99eaecc66e44abdfb2e50df34a2e
Peter <phefland@gmail.com>
2019-08-25 18:54:38 -0300

    Added Banner component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { HomeHeader } from "../utils"
+import { HomeHeader, Banner } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <HomeHeader img={img}>Hello from HomeHeader</HomeHeader>
+    <HomeHeader img={img}>
+      <Banner title="eatery" subtitle="77 main street - Rio de Janeiro, RJ" />
+    </HomeHeader>
   </Layout>
 )
 
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,13 +3,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { PageHeader } from "../utils"
+import { PageHeader, Banner } from "../utils"
 import menuImg from "../images/bcg/menuBcg.jpeg"
 
 const MenuPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <PageHeader img={menuImg}>hello from Menu</PageHeader>
+    <PageHeader img={menuImg}>
+      <Banner title="Our Menu" subtitle={`Let's dig in`} />
+    </PageHeader>
   </Layout>
 )
 
