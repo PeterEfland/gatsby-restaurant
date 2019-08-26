f021cd31955ead33a9db976e86d0e92384fcec7f
Peter <phefland@gmail.com>
2019-08-25 21:29:53 -0300

    added Banner Button and finished navbar and Banner component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,19 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { HomeHeader, Banner } from "../utils"
+import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
+import QuickInfo from "../components/HomePageComponents/QuickInfo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader img={img}>
-      <Banner title="eatery" subtitle="77 main street - Rio de Janeiro, RJ" />
+      <Banner title="eatery" subtitle="77 main street - Rio de Janeiro, RJ">
+        <BannerButton style={{ margin: "2rem auto" }}>Menu</BannerButton>
+      </Banner>
     </HomeHeader>
+    <QuickInfo />
   </Layout>
 )
 
